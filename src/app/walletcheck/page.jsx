dae9be850645496337f7aa87de21865156f14419
Vlad Cervac <vladcervac9@gmail.com>
2024-12-12 13:40:28 +0100

    add new aml check passed:

--- src/app/walletcheck/page.jsx
+++ src/app/walletcheck/page.jsx
@@ -1,10 +1,12 @@
 import { WalletProvider } from "@/app/context/globalContext";
 import Wrapper from "@/app/walletcheck/WrapperComponent";
+import AmlCheckPassed from "./AmlCheckPassed";
 
 export default function WalletCheckPage() {
   return (
     <WalletProvider>
       <Wrapper />
+      
     </WalletProvider>
   );
 }
